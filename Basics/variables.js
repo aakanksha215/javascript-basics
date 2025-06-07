184320170d18e3b94f60883eb524efe8db16bd95
Aakanksha Chamoli <97774809+aakanksha215@users.noreply.github.com>
2025-06-07 15:25:39 +0000

    added variables.js

---
+++ Basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 397612
+let accountEmail = "abc@gmail.com" 
+var accountPassword = "230UN6"
+accountCity = "Dehradun"
+let accountState;
+
+// we can never change const datatype 
+// We can change let 
+// We can change var
+
+/*
+var -> issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
