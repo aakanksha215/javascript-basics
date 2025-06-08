a69d02f4a1d76669b1392fbed7266f7365126963
Aakanksha Chamoli <97774809+aakanksha215@users.noreply.github.com>
2025-06-08 05:44:27 +0000

    added conversion.js and operation.js

---
+++ Basics/conversion.js
@@ -0,0 +1,29 @@
+let score = "86abn"
+let bool = true
+
+console.log(typeof score)
+
+let value = Number(score)
+console.log(typeof value)
+console.log(value)
+
+let boolValue = Number(bool)
+console.log(typeof boolValue)
+console.log(boolValue)
+
+// "33" => 34
+// "55abd" => NaN
+// true = 1
+
+let loggedIn = ""
+let isLoggedIn = Boolean(loggedIn)
+console.log(isLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "aakanksha"=> true
+
+let someNumber = 45
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ Basics/operations.js
@@ -0,0 +1,18 @@
+let value = 7
+let negValue = -value
+console.log(negValue)
+
+console.log(4+4)
+console.log(4-4)
+console.log(4*4)
+console.log(4**4)
+
+let str1 = "javascript"
+let str2 = "basics"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2) // 12
+console.log(1+"2") // 12
+console.log("1"+2+2) // 122
+console.log(1+2+"2") // 32
