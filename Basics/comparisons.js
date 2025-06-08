8963af155fce449d2a7c7caf05a913b8edc08535
Aakanksha Chamoli <97774809+aakanksha215@users.noreply.github.com>
2025-06-08 07:17:48 +0000

    added conversons.js and some change to operations.js

---
+++ Basics/comparisons.js
@@ -0,0 +1,20 @@
+console.log(2 > 1)
+console.log(2 < 1)
+console.log(2 == 1)
+console.log(2 <= 1)
+console.log(2 >= 1)
+console.log(2 != 1)
+
+console.log("2" > 66)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// working of == is different to that of >=
+
+console.log(undefined == 0)
+
+
+// === it strictly check (check both value and datatype)
+console.log("7" == 7)
--- Basics/operations.js
+++ Basics/operations.js
@@ -16,3 +16,13 @@ console.log("1"+2) // 12
 console.log(1+"2") // 12
 console.log("1"+2+2) // 122
 console.log(1+2+"2") // 32
+
+console.log(true)
+console.log(+true) // output => 1
+console.log(+"") // 0
+
+let num1, num2, num3
+
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter)
