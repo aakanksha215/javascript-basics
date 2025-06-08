791b5fc92a9c35179f2caefb4a01e4fc394afc14
Aakanksha Chamoli <97774809+aakanksha215@users.noreply.github.com>
2025-06-08 02:49:16 +0000

    added datatypes

---
+++ Basics/datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treate old js as new version
+
+//alert(2+5) // can be used on browser
+
+let name = "Aaksnksha"
+let a = 90
+let bool = false
+
+console.log(typeof "Aaka")
+
+// number
+// bigint
+// string
+// boolean
+// null => representation of empty value (null ka type objects hota ha)
+let state = null
+
+// undefined
+// symbol => unique
+
+/* user defined datatypes---> */
+
+// objects
